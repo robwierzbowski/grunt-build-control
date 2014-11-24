cf7784f673e244a5fb390ce8b19607740de7f4b9
Kevin Woo <kevinawoo@gmail.com>
2014-11-24 02:23:55 -0600

    (tests) don't show secure info

---
+++ test/scenarios/secure endpoint/gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+  // add custom tasks
+  // NOTE: cwd is `test/mock-repo`
+  grunt.loadTasks('../../tasks');
+
+
+  // test config
+  grunt.initConfig({
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        remote: 'https://github.com/pubUsername/temp.git',
+        login: 'privateUsername',
+        token: '1234567890abcdef',
+        stream: true,
+        connectCommits: false
+      },
+      deploy: {
+        options: {
+          branch: 'master',
+          commit: true,
+          message: 'simple deploy commit message',
+          push: true
+        }
+      }
+    }
+  });
+
+  // default task
+  grunt.registerTask('default', ['buildcontrol']);
+};
